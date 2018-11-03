7c4160b1876755a60cac09102c61bc891af2d8e5
Nicolas Mahe <nicolas@mahe.me>
2018-11-03 19:54:53 +0100

    update smart contract

--- services/api-contract/contracts/Api.sol
+++ services/api-contract/contracts/Api.sol
@@ -1,19 +1,57 @@
 pragma solidity ^0.4.23;
 
 contract Api {
-    address public owner;
-    mapping (bytes32=>uint) verifications;
-
-    event APIexecute(bytes32 id, string data);
-
-    constructor() public {
-        owner = msg.sender;
+    enum Status { Created, Success, Error }
+    struct Execution {
+        Status status;
+        string result;
+        address sender;
     }
 
-    function execute(string data) public payable {
-        bytes32 id = keccak256(
+    address public owner;
+    uint256 public price;
+    mapping (bytes32=>Execution) executions;
+
+    event ExecutionCreated(bytes32 indexed id, string data);
+    event ExecutionChanged(bytes32 indexed id);
+
+    constructor(uint256 _price) public {
+        owner = msg.sender;
+        price = _price;
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender == owner) _;
+    }
+
+    function execute(string data) public payable returns (bytes32 id) {
+        require(msg.value >= price, "Not enough value");
+        bytes32 _id = keccak256(
             abi.encodePacked(msg.sender, block.number, data)
         );
-        emit APIexecute(id, data);
+        executions[_id] = Execution({
+            status: Status.Created,
+            result: "",
+            sender: msg.sender
+        });
+        emit ExecutionCreated(_id, data);
+        return _id;
+    }
+
+    function editExecution(bytes32 id, Status status, string result) public onlyOwner {
+        Execution storage exec = executions[id];
+        exec.status = status;
+        exec.result = result;
+
+        if (status == Status.Success) {
+            // transfer price to owner
+            owner.transfer(price);
+        }
+        else {
+            // transfer price back to sender
+            exec.sender.transfer(price);
+        }
+
+        emit ExecutionChanged(id);
     }
 }
