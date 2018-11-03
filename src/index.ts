d489123289a7b3d7c0a8938a1f239b1250221fbc
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-11-03 22:48:29 +0100

    call result of execution

--- src/index.ts
+++ src/index.ts
@@ -11,13 +11,13 @@ const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY
 const ETHEREUM_ID = process.env.ETHEREUM_ID
 const ENCRYPT_ID = process.env.ENCRYPT_ID
 const API_ID = process.env.API_ID
-const ETH_PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETH_PRIVATE_KEY = process.env.ETH_PRIVATE_KEY
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const PASSPHRASE = process.env.PASSPHRASE
-const API_CONTRACT_ABI = require('../services/api-contract/build/contracts/Api.json').abi
-const API_CALL_ABI = API_CONTRACT_ABI.filter((x: any) => x.type === 'event' && x.name === 'APIexecute')[0]
-const SUBMIT_VERIFICATION_ABI = API_CONTRACT_ABI.filter((x: any) => x.type === 'function' && x.name === 'submitVerification')[0]
+const API_CONTRACT_ABI = require('../services/api-contract/Api.json')
+const API_CALL_ABI = API_CONTRACT_ABI.filter((x: any) => x.type === 'event' && x.name === 'ExecutionCreated')[0]
+const EDIT_EXECUTION_ABI = API_CONTRACT_ABI.filter((x: any) => x.type === 'function' && x.name === 'editExecution')[0]
 
 const MESG = application()
 
@@ -43,7 +43,7 @@ MESG.whenResult({
 }, {
   serviceID: ENCRYPT_ID,
   taskKey: 'decrypt',
-  tags: ['decrypt'],
+  tags: (outputKey: string, outputData: any, taskKey: string, tags: string[]) => [...tags, `id=${outputData.decodedData.id}`, 'decrypt'],
   inputs: (outputKey: string, outputData: any, taskKey: string, tags: string[]) => ({
     privateKey: PRIVATE_KEY,
     passphrase: PASSPHRASE,
@@ -59,9 +59,9 @@ MESG.whenResult({
 }, {
   serviceID: API_ID,
   taskKey: 'send',
+  tags: (outputKey: string, outputData: any, taskKey: string, tags: string[]) => [...tags, 'send'],
   inputs: (outputKey: string, outputData: any, taskKey: string, tags: string[]) => {
     const data = JSON.parse(outputData.data)
-    console.log(data)
     return {
       apiKey: SENDGRID_API_KEY,
       from: data.from || 'contact@mesg.com',
@@ -72,19 +72,29 @@ MESG.whenResult({
   }
 })
 
-// MESG.whenResult({
-//   serviceID: API_ID,
-//   taskKey: 'send',
-//   outputKey: 'success'
-// }, {
-//   serviceID: ETHEREUM_ID,
-//   taskKey: 'executeSmartContractMethod',
-//   inputs: (outputKey: string, outputData: any, taskKey: string, tags: string[]) => ({
-//     methodAbi: SUBMIT_VERIFICATION_ABI,
-//     contractAddress: CONTRACT_ADDRESS,
-//     privateKey: ETH_PRIVATE_KEY,
-//     data: {
-//       status: outputData.status.toString()
-//     }
-//   })
-// })
+MESG.whenResult({
+  serviceID: API_ID,
+  taskKey: 'send',
+  outputKey: 'success',
+  tagFilters: ['send']
+}, {
+  serviceID: ETHEREUM_ID,
+  taskKey: 'executeSmartContractMethod',
+  inputs: (outputKey: string, outputData: any, taskKey: string, tags: string[]) => {
+    const id = tags
+      .filter(x => x.startsWith('id='))
+      .map(x => x.split('=')[1])
+      .join('')
+    return {
+      methodAbi: EDIT_EXECUTION_ABI,
+      contractAddress: CONTRACT_ADDRESS,
+      privateKey: ETH_PRIVATE_KEY,
+      gasLimit: 8000000,
+      inputs: {
+        id,
+        status: 1,
+        result: outputData.status.toString()
+      }
+    }
+  }
+})
