67e8fd7bc68e61262d2672ba49a3804f421c9d59
Benz <ruthamnong.thotsaphon@gmail.com>
2020-01-06 15:56:27 +0700

    remove services/encryption

--- services/encryption/index.js
+++
@@ -1,7 +0,0 @@
-const MESG = require('mesg-js').service()
-
-MESG.listenTask({
-  encrypt: require('./tasks/encrypt'),
-  decrypt: require('./tasks/decrypt'),
-  generate: require('./tasks/generate')
-})
